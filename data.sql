579b60081665ceca643aeed6719918754f54a115
jitae-moon <jitae.moon@icloud.com>
2024-07-16 15:51:22 +0900

    기본 쿼리 추가.

--- data.sql
+++ data.sql
@@ -1,15 +1,27 @@
 <!-- DDL -->
-CREATE TABLE `comment` (
-
+CREATE TABLE `COMMENT` (
+    `id` bigint(19) NOT NULL AUTO_INCREMENT,
+    `user_id` varchar(32) NOT NULL,
+    `reg_date` datetime NOT NULL DEFAULT NOW(),
+    `content` mediumtext,
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `reply` (
-
+CREATE TABLE `REPLY` (
+    `id` bigint(19) NOT NULL AUTO_INCREMENT,
+    `comment_id` bigint(19) NOT NULL,
+    `user_id` varchar(32) NOT NULL,
+    `content` mediumtext,
+    `reg_date` datetime DEFAULT NOW(),
+    PRIMARY KEY (`id`)
 );
 
-<!-- USER -->
-CREATE TABLE `user` (
+CREATE TABLE `USER` (
     `id` varchar(32) NOT NULL,
     `name` varchar(100) NOT NULL,
     PRIMARY KEY (`id`)
 );
+
+<!-- DML -->
+INSERT INTO COMMENT (`user_id`, `content`)
+VALUES ('test1', 'test1 content');
